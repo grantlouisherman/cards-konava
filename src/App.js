abe8dd725d77bb30397f3e109c1e6a160c5b2568
grantlouisherman <grantlouisherman041@gmail.com>
2020-07-19 14:50:00 -0400

    react router basic setup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
 import Canvas from "./Components/Canvas";
+import RouterConfig from "./RouterConfig";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Canvas />
-      </div>
+      <RouterConfig />
     </Provider>
   );
 }
---
+++ src/RouterConfig.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Canvas from "./Components/Canvas";
+import Home from "./Components/Home";
+
+const RouterConfig = () => {
+  return (
+    <Router>
+      <Switch>
+       <Route path="/canvas">
+        <Canvas/>
+       </Route>
+       <Route path="/">
+        <Home/>
+       </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default RouterConfig;
